fa965dd68347f760c5c9a7344c49e43ab57c64fd
h0l-d3n <47954343+h0l-d3n@users.noreply.github.com>
2021-03-11 16:42:58 +0100

    Update tw.js

--- tw.js
+++ tw.js
@@ -13,7 +13,7 @@ $("button").click(function(){
   $("p").hide();
   var typewriter = new Typewriter(app, {
     loop: false,
-    cursor: "_"
+    cursor: "-->"
   });
 
   typewriter.pauseFor(1500)
@@ -28,7 +28,7 @@ $("button").click(function(){
       .pauseFor(1500)
       .typeString('<br><p> Mention me on <a href="https://twitter.com/c0rm4n01" class="twitter-mention-button" data-show-count="false"><span>Twitter</span></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> if you want to get in touch.</p>')
       .pauseFor(1500)
-      .typeString('<br><p> Thanks for visiting my page.</p>')
+      .typeString('<br><p> Thanks for stopping by.</p>')
       .pauseFor(1500)
   typewriter.typeString('<br><p> Bye.</p> <br><img src="http://www.hackthebox.eu/badge/image/484328" alt="Hack The Box">\n')
       .start();
