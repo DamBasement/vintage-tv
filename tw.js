7de069342a67ced2dcfc8150dd969ed61dbffd5e
Dam Basement <47954343+DamBasement@users.noreply.github.com>
2022-01-20 16:09:58 +0100

    Update tw.js

--- tw.js
+++ tw.js
@@ -29,6 +29,6 @@ $("button").click(function(){
       .pauseFor(1500)
       .typeString('<br><p> Thanks for stopping by.</p>')
       .pauseFor(1500)
-  typewriter.typeString('<br><p>Over and Out.💔</p> <br>')
+  typewriter.typeString('<br><p>Over and Out.</p> <br>')
       .start();
 });
